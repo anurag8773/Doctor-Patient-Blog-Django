15a066962a3bf0448ba9bdaaf371b9c5c3f44f57
Anurag Singh <anurag21xxx005@akgec.ac.in>
2023-12-12 02:27:59 +0530

    file added

--- signin_signup_django/settings.py
+++ signin_signup_django/settings.py
@@ -125,10 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'Doctor-Patient-Blog-Django/static/'),
+    os.path.join(BASE_DIR / 'static'),
 ]
 
 # Default primary key field type
