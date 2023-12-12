ff5ad1fcc51323267b3c381a4c2edde6541cc17d
Anurag Kumar Maurya <113245724+anurag8773@users.noreply.github.com>
2023-12-12 09:36:36 +0530

    Update urls.py

--- task1/urls.py
+++ task1/urls.py
@@ -18,6 +18,3 @@ urlpatterns = [
     path('blog/viewblog/<int:bno>',viewblog),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-if settings.DEBUG:
- urlpatterns += path('admin/', admin.site.urls)
